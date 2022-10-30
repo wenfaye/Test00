a73f07ca61446f0bf8cc37c73dc681a3d72fdb2c
wenfaye <thesims4@126.com>
2022-10-30 21:10:33 +0800

    edit

---
+++ Demo02/vue2_test/src/components/Computed.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div>
+        姓：<input type="text" v-model="firstName"> <br/><br/>
+			名：<input type="text" v-model="lastName"><br/><br/>
+			<span>全名:{{fullName}}</span><br/><br/>
+			<span>全名:{{fullName}}</span><br/><br/>
+			全名: <input type="text" v-model="fullName">
+
+            <div class="wrap"></div>
+
+    </div>
+
+    
+</template>
+<script>
+export default {
+    name: 'computedComponent',
+    data() {
+        return {
+            firstName: '张',
+            lastName: '三',
+        }
+    },
+    computed:{
+        fullName(){
+            return this.firstName +'-'+ this.lastName;
+        }
+    }
+}
+</script>
+<style>
+   .wrap{
+    width: 169px;
+    height: 169px;
+    margin-top: 10px;
+    background: url(../assets/pic/00.png) no-repeat;
+    background-size: cover;
+    position: relative;
+   } 
+   .wrap::after{
+    content: '';
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 169px;
+    height: 169px;
+    background: url(../assets/pic/03.png) no-repeat;
+    background-size: cover;
+    mask: linear-gradient(90deg,#000 35%,transparent)
+   }
+</style>
---
+++ Demo02/vue2_test/src/components/Event.vue
@@ -0,0 +1,29 @@
+<template lang="">
+    <div>
+        <h2>欢迎来到{{school}}学习</h2>
+        <button @click="show2(5,$event)">press</button>
+        <p><a href="https://weibo.com/" @click.prevent="show3">点我提示：信息3 （阻止默认行为）</a></p>
+    </div>
+</template>
+<script>
+export default {
+    name:'EventComponent',
+    data(){
+        return{
+            school:'尚硅谷',
+        }
+    },
+    methods: {
+        show2(num,event){
+            console.dir(event);
+            console.log(num);
+        },
+        show3(event){
+            console.dir(event);
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
